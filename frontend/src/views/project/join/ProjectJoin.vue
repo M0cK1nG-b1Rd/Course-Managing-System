cb24e8a9adbebdde0575b01ba1b2fc1318c4cb03
wujianP <49806658+wujianP@users.noreply.github.com>
2020-12-30 17:59:09 +0800

    添加一个修改

--- frontend/src/views/project/join/ProjectJoin.vue
+++ frontend/src/views/project/join/ProjectJoin.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-    在此加入项目
-  </div>
+  <el-cascader :props="props"></el-cascader>
 </template>
 
 <script>
+let id = 0;
+
 export default {
-  name: 'ProjectJoin'
-}
+  data() {
+    return {
+      props: {
+        lazy: true,
+        lazyLoad (node, resolve) {
+          const { level } = node;
+          setTimeout(() => {
+            const nodes = Array.from({ length: level + 1 })
+              .map(item => ({
+                value: ++id,
+                label: `选项${id}`,
+                leaf: level >= 2
+              }));
+            // 通过调用resolve将子节点数据返回，通知组件数据加载完成
+            resolve(nodes);
+          }, 1000);
+        }
+      }
+    };
+  }
+};
 </script>
-
-<style scoped>
-
-</style>
